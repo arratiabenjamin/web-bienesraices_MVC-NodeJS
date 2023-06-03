e2a7da6c32cfb519aa8cbcf73afefe3dd87e1c86
arratiabenjamin <benjamin.arratia.santibanez@gmail.com>
2023-06-03 00:36:12 -0400

    Creacion de Pin

--- public/js/mapa.js
+++ public/js/mapa.js
@@ -16,7 +16,7 @@
   \************************/
 /***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n(function() {\n\n    //Latitud\n    const lat = -33.45694;\n    //Longitud\n    const lng = -70.64827;\n    //Asignacion de valores\n    const mapa = L.map('mapa').setView([lat, lng ], 12);\n    \n    //Creacuin de Mapa\n    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {\n        attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors'\n    }).addTo(mapa);\n\n\n})()\n\n//# sourceURL=webpack://bienesraices_mvc/./src/js/mapa.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n(function() {\n\n    //Latitud\n    const lat = -33.45694;\n    //Longitud\n    const lng = -70.64827;\n    //Asignacion de valores\n    const mapa = L.map('mapa').setView([lat, lng ], 12);\n    //var de Pincho\n    let marker;\n    \n    //Creacuin de Mapa\n    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {\n        attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors'\n    }).addTo(mapa);\n\n    //Pincho\n    marker = new L.marker([lat, lng], {\n        //Mover Pincho\n        draggable: true,\n        //Reposicionar Mapa\n        autoPan: true\n    })\n    .addTo(mapa)\n\n})()\n\n//# sourceURL=webpack://bienesraices_mvc/./src/js/mapa.js?");
 
 /***/ })
 
