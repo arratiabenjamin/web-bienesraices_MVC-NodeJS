1f49993ed2bddc0710545c91b471ab5da7671387
arratiabenjamin <benjamin.arratia.santibanez@gmail.com>
2023-06-06 23:26:20 -0400

    Creacion Modelos Categoria y Precio - Implementacion de Seed

---
+++ seed/seeder.js
@@ -0,0 +1,54 @@
+import categorias from "./categorias.js";
+import precios from "./precios.js";
+import Categoria from "../models/Categoria.js";
+import Precio from "../models/Precio.js"
+import db from "../config/db.js";
+
+const importarDatos = async () => {
+    try {
+        //Autenticar
+        await db.authenticate();
+
+        //Generar Columnas
+        await db.sync();
+
+        //Inserta los Datos
+        //Promise.all - Es util usarlo si cada await no depende del otro,
+        //              Si depende de Otro se Realiza por Separado.
+        await Promise.all([
+            Categoria.bulkCreate(categorias),
+            Precio.bulkCreate(precios)
+        ])
+
+        console.log('Datos Importados Correctamente.');
+        process.exit(); //No se pasa codigo cuando fue correcto.
+        
+    } catch (error) {
+        console.log(error);
+        process.exit(1); //Se pasa codigo (1) si fue un error.
+    }
+}
+const eliminarDatos = async () => {
+    try {
+        //Forma menos "Invasiva" (SOlo ELimina los Datos y reinicia los IDs)
+        // await Promise.all([
+        //     Categoria.destroy({where: {}, truncate: true}),
+        //     Precio.destroy({where: {}, truncate: true})
+        // ])
+
+        //Forma mas Invasiva (Elimina y vuelve a crear las Tablas)
+        await db.sync({force: true});
+        console.log('Datos Eliminados Correctamente');
+        process.exit();
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+//argv son como se pasan Argumentos a una Variable desde la Terminal
+if(process.argv[2] === "-i"){
+    importarDatos();
+}
+if(process.argv[2] === "-d"){
+    eliminarDatos();
+}
