d51615305e6288b3f1f2e6f368406dda10e009b3
arratiabenjamin <benjamin.arratia.santibanez@gmail.com>
2023-06-12 23:08:32 -0400

    Importacion y Muestra de Precios y Categorias

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,14 +1,25 @@
 
+import Precio from "../models/Precio.js";
+import Categoria from "../models/Categoria.js";
+
 const admin = (req, res) => {
     res.render('propiedades/admin', {
         pagina: 'Mis Propiedades',
         barra: true
     });
 }
-const crear = (req, res) => {
+const crear = async (req, res) => {
+    //Consultar Modelo de Precio y Categoria
+    const [categorias, precios] = await Promise.all([ //Realiza todas las acciones en 1 solo await
+        Categoria.findAll(),
+        Precio.findAll()
+    ]);
+
     res.render('propiedades/crear', {
         pagina: 'Crear Propiedad',
-        barra: true
+        barra: true,
+        categorias, //ObjectLiteral
+        precios
     });
 }
 
